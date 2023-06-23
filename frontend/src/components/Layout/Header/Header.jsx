a78a8176fbcb364ea529fa844437bbfadb6aceb3
titine63 <sblanchonnet@yahoo.fr>
2023-06-23 11:46:30 +0200

    dessin

--- frontend/src/components/Layout/Header/Header.jsx
+++ frontend/src/components/Layout/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
+import logo from "./logo4.png";
 
 export default function Header() {
   return (
     <header>
-      <h1 className="logo">AlliEZ</h1>
+      <div className="logo-container">
+        <img src={logo} alt="Logo AlliEZ" className="logo-image" />
+        <h1 className="logo-text">AlliEZ</h1>
+      </div>
       <nav>
         <Link to="/">HOME</Link>
         <Link to="/accommodation">HEBERGEMENT</Link>
